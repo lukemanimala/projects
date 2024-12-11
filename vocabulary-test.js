c24d69659f25a10bacfb69718d439e1cc555d96d
Luke  Manimala <lukemanimala@MacBookPro.attlocal.net>
2024-12-10 23:20:52 -0600

    Regenerated vocabulary.js with consistent formatting

---
+++ vocabulary-test.js
@@ -0,0 +1,61 @@
+const vocabulary = [
+    {
+        verb: "Aceptar",
+        translation: "to accept",
+        presente: [
+            { conjugation: "acepto", definition: "I accept" },
+            { conjugation: "aceptas", definition: "You (informal) accept" },
+            { conjugation: "acepta", definition: "He/She accepts" },
+            { conjugation: "aceptamos", definition: "We accept" },
+            { conjugation: "aceptáis", definition: "You all (informal) accept" },
+            { conjugation: "aceptan", definition: "They/You all accept" }
+        ],
+        preterito: [
+            { conjugation: "acepté", definition: "I accepted" },
+            { conjugation: "aceptaste", definition: "You (informal) accepted" },
+            { conjugation: "aceptó", definition: "He/She accepted" },
+            { conjugation: "aceptamos", definition: "We accepted" },
+            { conjugation: "aceptasteis", definition: "You all (informal) accepted" },
+            { conjugation: "aceptaron", definition: "They/You all accepted" }
+        ],
+        futuro: [
+            { conjugation: "aceptaré", definition: "I will accept" },
+            { conjugation: "aceptarás", definition: "You (informal) will accept" },
+            { conjugation: "aceptará", definition: "He/She will accept" },
+            { conjugation: "aceptaremos", definition: "We will accept" },
+            { conjugation: "aceptaréis", definition: "You all (informal) will accept" },
+            { conjugation: "aceptarán", definition: "They/You all will accept" }
+        ]
+    },
+    {
+        verb: "Hablar",
+        translation: "to speak",
+        presente: [
+            { conjugation: "hablo", definition: "I speak" },
+            { conjugation: "hablas", definition: "You (informal) speak" },
+            { conjugation: "habla", definition: "He/She speaks" },
+            { conjugation: "hablamos", definition: "We speak" },
+            { conjugation: "habláis", definition: "You all (informal) speak" },
+            { conjugation: "hablan", definition: "They/You all speak" }
+        ],
+        preterito: [
+            { conjugation: "hablé", definition: "I spoke" },
+            { conjugation: "hablaste", definition: "You (informal) spoke" },
+            { conjugation: "habló", definition: "He/She spoke" },
+            { conjugation: "hablamos", definition: "We spoke" },
+            { conjugation: "hablasteis", definition: "You all (informal) spoke" },
+            { conjugation: "hablaron", definition: "They/You all spoke" }
+        ],
+        futuro: [
+            { conjugation: "hablaré", definition: "I will speak" },
+            { conjugation: "hablarás", definition: "You (informal) will speak" },
+            { conjugation: "hablará", definition: "He/She will speak" },
+            { conjugation: "hablaremos", definition: "We will speak" },
+            { conjugation: "hablaréis", definition: "You all (informal) will speak" },
+            { conjugation: "hablarán", definition: "They/You all will speak" }
+        ]
+    }
+    // Add more verbs as needed
+];
+
+export default vocabulary;
